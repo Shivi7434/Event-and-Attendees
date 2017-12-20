c33f65a7b60938f2d6d3a8af0e5bbce9166c63fe
Shivi7434 <shivalikhanuja.net@gmail.com>
2017-12-20 07:01:56 -0600

    first commit

---
+++ app/controllers/attends_controller.rb
@@ -0,0 +1,22 @@
+class AttendsController < ApplicationController
+
+    def create
+        event = Event.find(params[:attend][:event_id])
+        Attend.create(attend_params) unless event.attendees.include? current_user
+
+        return redirect_to :back
+    end
+
+    def destroy
+        @organization = Organization.find(params[:id])
+        
+        @organization.destroy
+
+    end 
+       
+    private
+        def attend_params
+            params.require(:attend).permit(:event_id).merge(user: current_user)
+        end    
+
+end
